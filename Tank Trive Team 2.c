26834acc17915bbc3746d69ee5b25cbeb29f4ec2
alexdandrea11@gmail.com <alexdandrea11@gmail.com@43e9b7cf-090a-3510-c551-e08e6667785f>
2012-11-28 01:17:54 +0000

    Today i got the tank drive working for the 2nd teams robot.
    git-svn-id: https://vex2713.googlecode.com/svn/trunk@28 43e9b7cf-090a-3510-c551-e08e6667785f

--- Tank Trive Team 2.c
+++ Tank Trive Team 2.c
@@ -2,6 +2,21 @@
 task main()
 {
 
+bMotorReflected[port1] = false;
+bMotorReflected[port10] = true;
+bMotorReflected[port2] = true;
+bMotorReflected[port9] = false;
 
+while (true)
+
+{
+
+motor[port1] = vexRT[Ch3];
+motor[port2] = vexRT[Ch3];
+motor[port9] = vexRT[Ch2];
+motor[port10] = vexRT[Ch2];
+
+
+}
 
 }
