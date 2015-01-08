b102904e4f8ccc8e75b90a6d28cf821c6316a6bc
conlanc@gmail.com <conlanc@gmail.com@43e9b7cf-090a-3510-c551-e08e6667785f>
2015-01-08 23:21:09 +0000

    Added possible WHEEL_TYPE variable for more accurate measurements of potentiometers.
    git-svn-id: https://vex2713.googlecode.com/svn/trunk@123 43e9b7cf-090a-3510-c551-e08e6667785f

--- 2014-15/B Bot/Main.c
+++ 2014-15/B Bot/Main.c
@@ -25,12 +25,8 @@
 
 void pre_auton()
 {
-	// Set bStopTasksBetweenModes to false if you want to keep user created tasks running between
-	// Autonomous and Tele-Op modes. You will need to manage all user created tasks if set to false.
 	bStopTasksBetweenModes = true;
-
-	// All activities that occur before the competition starts
-	// Example: clearing encoders, setting servo positions, ...
+	word WHEEL_TYPE = omni;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////
